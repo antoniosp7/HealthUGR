7ad19f26b0cd3b1177dd9d0c89f1921a07762d46
antoniosp7 <antoniosalvatperez@gmail.com>
2020-11-20 00:17:27 +0100

    change class Record Close #40

--- src/main/scala/Record.scala
+++
@@ -1,7 +0,0 @@
-import Food.Food
-import Exercise.Exercise
-
-package record {
-    class Record(var food: List[Food], var exercises: List[Exercise], var caloriesConsumed: Int, var caloriesSpent: Int)
-}
-
