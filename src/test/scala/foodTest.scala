a0feb862b962be8ea9b521414ed5e7d99b40aeb4
antoniosp7 <antoniosalvatperez@gmail.com>
2020-11-20 11:19:22 +0100

    create test of add food by admin Close #34

--- src/test/scala/foodTest.scala
+++ src/test/scala/foodTest.scala
@@ -4,11 +4,24 @@ import Food.Food
 class testFood extends munit.FunSuite {
 
   var nutrients = List("test","test")
+  var nutrientsEmpty = List()
   var food = new Food(45, "test", nutrients)
   val foodController: FoodController = FoodController(food)
   
   test("addFood") {
-    assert(foodController.addFood(3, "test", nutrients) != false)
+    assert(clue(foodController.addFood(3, "test", nutrients)) == clue(true))
+  }
+
+  test("addFoodEmptyNutrients") {
+    assert(clue(foodController.addFood(3, "test", nutrientsEmpty)) == clue(false))
+  }
+
+   test("addFood0Calories") {
+    assert(clue(foodController.addFood(0, "test", nutrients)) == clue(false))
+  }
+
+   test("addFoodEmptyDescription") {
+    assert(clue(foodController.addFood(3, "", nutrients)) == clue(false))
   }
     
   
