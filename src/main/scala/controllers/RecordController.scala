8ebfbf2ec254eda569f3f4918e73cdcaa3cb421a
antoniosp7 <antoniosalvatperez@gmail.com>
2020-11-20 11:37:21 +0100

    change method to add the calories consumed Close #42

--- src/main/scala/controllers/RecordController.scala
+++ src/main/scala/controllers/RecordController.scala
@@ -5,11 +5,15 @@ import Food.Food
 
 case class RecordController private() {
 
-    def addFoodToRecord(record: Record, food: Food) : Boolean = {
+    def addFoodToRecord(record: Record, food: Food, grams: Int) : Boolean = {
         var res = false
         record.food(food) = food.caloriesPer100gr
+        var caloriesConsumed = (food.caloriesPer100gr * grams) / 100
+        record.caloriesConsumed += caloriesConsumed
         res = true
         res
     }
+
+    
   
 }
